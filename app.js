060b20a90d84e91b3039683c357c64db796ad695
Dan Ingamells <dan_ingamells@hotmail.co.uk>
2018-07-18 15:30:58 +0100

    v2

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-employee_directory_v1
+const data = $.ajax({
+  url: 'https://randomuser.me/api/?results=16',
+  dataType: 'json',
+  success: function(data) {
+    console.log(data);
+    for (i=0; i < data.info.results; i++) {
+    const firstName = data.results[i].name.first;
+    const lastName = data.results[i].name.last;
+    const email = data.results[i].email
+    birthDay
+    phoneNumber
+    address
+    postcode
+    console.log(firstName);
 
-
-/Users/INDC/Documents/GitHub/employee_directory_v1
+  }}
+});
