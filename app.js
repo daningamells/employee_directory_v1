3d0810348ab110941e0287c6ae90105d3962a9d8
Dan Ingamells <dan_ingamells@hotmail.co.uk>
2018-07-19 18:42:29 +0100

    v1

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var html;
+var person = [];
+
+
 const data = $.ajax({
   url: 'https://randomuser.me/api/?results=16',
   dataType: 'json',
@@ -7,33 +11,42 @@ const data = $.ajax({
     const firstName = data.results[i].name.first;
     const lastName = data.results[i].name.last;
     const email = data.results[i].email;
-    const birthDay = data.results[i].dob.date;
+    const birthDay = data.results[i].dob.date.substring(0,10);
     const phoneNumber = data.results[i].cell;
     const street = data.results[i].location.street;
     const state = data.results[i].location.state;
     const city = data.results[i].location.city;
     const postcode = data.results[i].location.postcode;
     const picture = data.results[i].picture.large;
+    person[i] = [firstName, lastName, email, birthDay, phoneNumber, street, state, city, postcode, picture];
 
 
-    const html = `<div class='person'><li><a href="${picture}" rel="lightbox" title="Designed by <a href='${picture}' target='_blank'>${firstName}</a>"><img src="${picture}" width="150" height="150"></a><p class='personTitle'>${firstName} ${lastName}</p><p class='subinfo'>${email}</p><p class='subinfo2'>${city}</p></li></div>`
+    html = `<div class='person' id='${i}'><li><a href="${picture}" rel="lightbox" title="Designed by <a href='${picture}' target='_blank'>${firstName}</a>"><img src="${picture}" width="150" height="150"></a><p class='personTitle'>${firstName} ${lastName}</p><p class='subinfo'>${email}</p><p class='subinfo2'>${city}</p><em><p class='phone'>${phoneNumber}</p><p class='address'>${street}, ${postcode}</p><p class='birthday'>Birthday: ${birthDay}</p></li></div>`
+
+
     $('.clearfix').append(html);
+
+
+
 }
+
+$('div.person').click(function(){
+  console.log('sdfsdfsd')
+  setTimeout(function(){
+  $('#jquery-lightbox').append(html);
+}, 300);
+});
+
 
 $('.personTitle').css('textTransform', 'capitalize');
 $('.subinfo2').css('textTransform', 'capitalize');
 
-$(function(){
-  $('a[rel=lightbox]').lightBox({
-    containerResizeSpeed: 250,
-    fixedNavigation: true
-  });
-});
+// $(function(){
+//   $('.person').lightBox({
+//     containerResizeSpeed: 250,
+//     fixedNavigation: false
+//   });
+// });
 }
-
-
-
-
-
 
 });
