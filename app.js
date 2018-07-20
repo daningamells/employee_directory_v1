05101e258c86f2a5d5a3e54be60fe6ef7f9944b7
Dan Ingamells <dan_ingamells@hotmail.co.uk>
2018-07-20 15:23:57 +0100

    v1

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-var html;
 var person = [];
-
-
+var html2;
 const data = $.ajax({
-  url: 'https://randomuser.me/api/?results=16',
+  url: 'https://randomuser.me/api/?results=16&nat=gb',
   dataType: 'json',
   success: function(data) {
     console.log(data);
@@ -20,33 +18,32 @@ const data = $.ajax({
     const picture = data.results[i].picture.large;
     person[i] = [firstName, lastName, email, birthDay, phoneNumber, street, state, city, postcode, picture];
 
-
-    html = `<div class='person' id='${i}'><li><a href="${picture}" rel="lightbox" title="Designed by <a href='${picture}' target='_blank'>${firstName}</a>"><img src="${picture}" width="150" height="150"></a><p class='personTitle'>${firstName} ${lastName}</p><p class='subinfo'>${email}</p><p class='subinfo2'>${city}</p><em><p class='phone'>${phoneNumber}</p><p class='address'>${street}, ${postcode}</p><p class='birthday'>Birthday: ${birthDay}</p></li></div>`
-
-
+    html = `<div class='person' id='${i}'><li><a href="#" rel="lightbox" title="Designed by <a href='${picture}' target='_blank'>${firstName}</a>"><img src="${picture}" width="150" height="150"></a><p class='personTitle'>${firstName} ${lastName}</p><p class='subinfo'>${email}</p><p class='subinfo2'>${city}</p></li></div>`
     $('.clearfix').append(html);
-
-
 
 }
 
-$('div.person').click(function(){
-  console.log('sdfsdfsd')
+$('div.person').click(function(e){
+
+  let pobj = $(this);
+  id = pobj["0"].id;
+  html2 = `<div class='personLB'> <a href="#" class="cancel">&times;</a><img class='imageLB' src="${person[id][9]}" width="150" height="150"></a><p class='personTitleLB'>${person[id][0]} ${person[id][1]}</p><p class='subinfoLBemail'>${person[id][2]}</p><p class='subinfo2LB'>${person[id][7]}</p><hr><span class='lbsub'><p class='phoneLB'>${person[id][4]}</p><p class='addressLB'>${person[id][5]}, ${person[id][8]}</p><p class='birthdayLB'>Birthday: ${person[id][3]}</p></span></div>`
+  $("#cover").css("display", "flex");
   setTimeout(function(){
-  $('#jquery-lightbox').append(html);
+  $('#cover').append(html2);
+  $('a.cancel').click(function(){
+  $("#cover").css("display", "none");
+  $('.personLB').remove();
+
+  });
 }, 300);
+
 });
 
 
+
 $('.personTitle').css('textTransform', 'capitalize');
-$('.subinfo2').css('textTransform', 'capitalize');
+$('.subinfo2, .subinfoLB').css('textTransform', 'capitalize');
 
-// $(function(){
-//   $('.person').lightBox({
-//     containerResizeSpeed: 250,
-//     fixedNavigation: false
-//   });
-// });
 }
-
 });
